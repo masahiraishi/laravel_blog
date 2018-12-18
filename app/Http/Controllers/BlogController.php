fd898d67dc05a0ae171720ae181642155900a580
平石匡 <masahiraishi22@gmail.com>
2018-12-18 21:17:36 +0900

    change:controll

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function getIndex()
+    {
+        $posts = Post::orderBy('id','desc')->pagintae(10);
+
+        $posts->getFactory()->setViewName('pagination::simple');
+        $this->layout->title = 'Laravelでブログ作成';
+        $this->layout->main = View::make('home')->nest('content', 'index', compact('posts'));
+    }
 }
